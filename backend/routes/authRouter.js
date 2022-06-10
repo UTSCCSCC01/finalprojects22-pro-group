e744470c8576ed4b67b7d35613c9a51403ec045b
oscarchen178 <65813281+oscarchen178@users.noreply.github.com>
2022-06-09 23:56:03 -0400

    PROG-49, added functions for register

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  login,
-  register,
-  see_all_users,
+    login,
+    register,
+    user_list,
+    logout,
+    profile,
 } = require("../controllers/auth");
 
-router.route("/").get(see_all_users);
+router.route("/").get(user_list);
 router.route("/register").post(register);
 router.route("/login").post(login);
-
+router.route("/logout").get(logout);
+router.route("/profile").get(profile);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -6,35 +6,43 @@ require("dotenv").config();
 const authRouter = require("./routes/authRouter");
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+const cookieParser = require("cookie-parser");
 
 // set up app
 const app = express();
 app.use(express.json());
-
-// temp page
-app.get('/', (req, res) => {
-  res.send('<h1>Pro View</h1><a href="/api/v1/register">register</a>');
+app.use(cookieParser());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
 });
 
-// routers 
-app.use("/api/v1", authRouter);
+// routers
+app.use("/api/", authRouter);
 
 // set up error
 app.use(notFound);
 // app.use(errorHandlerMiddleware);
 
 // set up port
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 // set up server
 const start = async () => {
-  try {
-    // connect mongodb database
-    await database_connect(process.env.PRO_VIEW_URI);
-    app.listen(port, () => console.log(`Server is listening port ${port}...`));
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        // connect mongodb database
+        await database_connect(process.env.PRO_VIEW_URI);
+        app.listen(port, () =>
+            console.log(`Server is listening port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
