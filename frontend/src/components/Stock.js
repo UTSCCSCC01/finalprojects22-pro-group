a9418580c65f7e8d71cc849dc897175566399112
Junheng <cw3141now@gmail.com>
2022-06-10 12:44:29 -0400

    PROG-58 removed unecessary files

--- frontend/src/components/Stock.js
+++ frontend/src/components/Stock.js
@@ -8,39 +8,50 @@ function Stock() {
   // const [StockSymbol, setStockSymbol] = useState("FB");
 
   const StockSymbol = "GOOG";
-  const API_KEY2 = "pk_2f2f1ecce08045ba9fe8824d485939bb";
+  const API_KEY = "THN5ITBH3LFSAWLV";
+
+  // let API_CALL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${StockSymbol}&output_size=compact&apikey=${API_KEY}`;
 
   const getStockRequest = async (StockSymbol) => {
-    const url2 = `https://cloud.iexapis.com/stable/stock/${StockSymbol}/chart/3m?token=${API_KEY2}`;
-    const url_volume =
-      "https://cloud.iexapis.com/stable/stock/aapl/book?token=pk_2f2f1ecce08045ba9fe8824d485939bb";
+    const check1 = localStorage.getItem("stockChartXValues");
+    const check2 = localStorage.getItem("stockChartYValues");
+    if (check1) {
+      setStockChartXValues(JSON.parse(check1));
+    }
+    if (check2) {
+      setStockChartYValues(JSON.parse(check2));
+    } else {
+      const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${StockSymbol}&output_size=compact&apikey=${API_KEY}`;
 
-    const response = await fetch(url2);
-
-    await response.json().then((data) => {
-      for (const key in data) {
-        console.log(data[key]["date"]);
-        setStockChartXValues((stockChartXValues) => [
-          ...stockChartXValues,
-          data[key]["date"],
-        ]);
-        setStockChartYValues((stockChartYValues) => [
-          ...stockChartYValues,
-          data[key]["close"],
-        ]);
-      }
-    });
+      const response = await fetch(url);
+      console.log(response);
+      await response.json().then((data) => {
+        for (const key in data["Time Series (Daily)"]) {
+          setStockChartXValues((stockChartXValues) => [
+            ...stockChartXValues,
+            key,
+          ]);
+          setStockChartYValues((stockChartYValues) => [
+            ...stockChartYValues,
+            data["Time Series (Daily)"][key]["1. open"],
+          ]);
+        }
+        localStorage.setItem("stockChartYValues", stockChartYValues);
+        localStorage.setItem("stockChartXValues", stockChartXValues);
+      });
+    }
   };
 
   useEffect(() => {
     getStockRequest(StockSymbol);
   }, []);
 
-  console.log(stockChartXValues);
+  stockChartXValues.slice(-1);
+
   return (
     <>
       <div>
-        <h1>Google</h1>
+        <h1>FB</h1>
         <Plot
           data={[
             {
