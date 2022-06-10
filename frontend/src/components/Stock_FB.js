49c39e85eac3acebf939cb8c7d2c2cdbf31c17ba
SimonLi1020 <55788824+SimonLi1020@users.noreply.github.com>
2022-06-09 22:57:35 -0400

    PROG-60 modified stock api

--- frontend/src/components/Stock_FB.js
+++ frontend/src/components/Stock_FB.js
@@ -1,71 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Plot from "react-plotly.js";
 
-class Stock_FB extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stockChartXValues: [],
-      stockChartYValues: [],
-    };
-  }
+function Stock() {
+  const [stockChartXValues, setStockChartXValues] = useState([]);
+  const [stockChartYValues, setStockChartYValues] = useState([]);
 
-  componentDidMount() {
-    this.fetchStock();
-  }
+  // const [StockSymbol, setStockSymbol] = useState("FB");
 
-  fetchStock() {
-    // const [StockSymbol, setStockSymbol] = useState("FB");
-    const StockSymbol = "FB";
-    // const StockSymbol_2= "FB";
-    const pointerToThis = this;
-    const API_KEY = "THN5ITBH3LFSAWLV";
+  const StockSymbol = "FB";
+  const API_KEY2 = "pk_2f2f1ecce08045ba9fe8824d485939bb";
 
-    let API_CALL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${StockSymbol}&output_size=compact&apikey=${API_KEY}`;
-    // let API_CALL_2 =  `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${StockSymbol_2}&output_size=compact&apikey=${API_KEY}`;
-    let stockChartXValuesFunction = [];
-    let stockChartYValuesFunction = [];
+  const getStockRequest = async (StockSymbol) => {
+    const url2 = `https://cloud.iexapis.com/stable/stock/${StockSymbol}/chart/3m?token=${API_KEY2}`;
+    const url_volume =
+      "https://cloud.iexapis.com/stable/stock/aapl/book?token=pk_2f2f1ecce08045ba9fe8824d485939bb";
 
-    fetch(API_CALL)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        for (var key in data["Time Series (Daily)"]) {
-          stockChartXValuesFunction.push(key);
-          stockChartYValuesFunction.push(
-            data["Time Series (Daily)"][key]["1. open"]
-          );
-        }
-        pointerToThis.setState({
-          stockChartXValues: stockChartXValuesFunction,
-          stockChartYValues: stockChartYValuesFunction,
-        });
-      });
-  }
+    const response = await fetch(url2);
 
-  render() {
-    return (
+    await response.json().then((data) => {
+      for (const key in data) {
+        console.log(data[key]["date"]);
+        setStockChartXValues((stockChartXValues) => [
+          ...stockChartXValues,
+          data[key]["date"],
+        ]);
+        setStockChartYValues((stockChartYValues) => [
+          ...stockChartYValues,
+          data[key]["close"],
+        ]);
+      }
+    });
+  };
+
+  useEffect(() => {
+    getStockRequest(StockSymbol);
+  }, []);
+
+  console.log(stockChartXValues);
+  return (
+    <>
       <div>
-        <h1>FB</h1>
+        <h1>FaceBook</h1>
         <Plot
           data={[
             {
-              x: this.state.stockChartXValues,
-              y: this.state.stockChartYValues,
+              x: stockChartXValues,
+              y: stockChartYValues,
               type: "scatter",
               mode: "lines+markers",
               marker: { color: "blue" },
             },
           ]}
-          layout={{ width: 720, height: 500, title: "StockSymbol" }}
+          layout={{
+            width: 720,
+            height: 500,
+            title: "StockSymbol",
+            xaxis: { title: "TIME" },
+            yaxis: { title: "COST" },
+          }}
         />
-        
-
       </div>
-    );
-  }
+    </>
+  );
 }
 
-
-export default Stock_FB;
+export default Stock;
