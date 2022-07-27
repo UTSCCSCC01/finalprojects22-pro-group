e6cff9c9709a35e799bea43ac282a43e4ac0bcfe
Qi123123Li <java.li@mail.utoronto.ca>
2022-07-27 19:10:21 -0400

    PROG-28 add news page

---
+++ frontend/src/components/News.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect} from "react";
+import axios from 'axios';
+import { TextField, Button } from "@mui/material";
+import "./News.css";
+import  NewsItem  from './NewsItem'
+import Sidebar from "./Sidebar";
+import Stock from "./Stock";
+import TAlert from "./alert";
+import Clock from "./Clock";
+
+import { ToastContainer } from "react-toastify";
+const News = () => {
+    const [articles, setArticles] = useState([])
+    const [input, setInput] = useState("");
+    const [search, setSearch] = useState("");
+    const [stockname, setStockname] = useState("");
+
+    useEffect(() => {
+        const getArticles = async () => {
+            const response = await axios.get(`https://newsapi.org/v2/everything?q=stockmarket&apiKey=2794e262e99e427d92f32dbeb4bdaccd`)
+            setArticles(response.data.articles)
+            console.log(response)
+        }
+
+        getArticles()
+    }, [])
+    return (
+        <div>
+            {articles.map(article => {
+                return(
+                    <NewsItem 
+                        title={article.title}
+                        description={article.description}
+                        url={article.url}
+                        urlToImage={article.urlToImage} 
+                    />
+                )
+            })}
+        </div>
+    )
+}
+
+export default News;
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -8,6 +8,7 @@ import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
 import SearchIcon from "@mui/icons-material/Search";
 import AlarmIcon from "@mui/icons-material/Alarm";
 import ShowChartIcon from "@mui/icons-material/ShowChart";
+import NewspaperIcon from '@mui/icons-material/Newspaper';
 import "./Sidebar.css";
 
 function Sidebar() {
@@ -32,7 +33,9 @@ function Sidebar() {
                 <SidebarOpt Icon={PeopleIcon} text="Community" />
                 <SidebarOpt Icon={PeopleIcon} text="Group" />
                 <SidebarOpt Icon={ShowChartIcon} text="Trading" />
+                <SidebarOpt Icon={NewspaperIcon} text="News" />
                 <SidebarOpt Icon={LogoutIcon} text="Logout" />
+
             </div>
         </div>
     );
