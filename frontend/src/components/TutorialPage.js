36ab8f58d9f3e7fddb4159476fc01737955ae5d4
SimonLi1020 <55788824+SimonLi1020@users.noreply.github.com>
2022-07-30 00:59:07 -0400

    PROG-33 add framework of the tutorial page

--- frontend/src/components/TutorialPage.js
+++ frontend/src/components/TutorialPage.js
@@ -1,11 +1,42 @@
 import React, { Fragment } from "react";
 import ResponsivePlayer from "./ResponsePlayer.jsx";
+import "./TutorialPage.css"
 
 const TutorialPage = () => {
     return (
-        <div className="TutorialPage">
-                <ResponsivePlayer></ResponsivePlayer>
-        </div>
+        <><div className="Header">
+            <h1>Tutorial Page</h1>
+        </div><div className="StockConcept">
+                
+                <div className="StockText">
+                    <h2>What is Stock?</h2>
+                    <h4>Stock is a security that represents the ownership of a fraction of the issuing corporation</h4>
+                    <h4>Stocks are bought and sold predominantly on stock exchanges and are the foundation of many individual investors' portfolios</h4>
+                </div>
+                <div className="StockVideo">
+                    <ResponsivePlayer give_url="https://www.youtube.com/watch?v=o4jfBC0AgIM"></ResponsivePlayer>
+                </div>
+
+                
+
+            </div>
+            <div className="StockMarketConcept">
+                
+                <div className="StockMarketText">
+                    <h2>What is Stock Market?</h2>
+                    <h4>Stock is a security that represents the ownership of a fraction of the issuing corporation</h4>
+                    <h4>Stocks are bought and sold predominantly on stock exchanges and are the foundation of many individual investors' portfolios</h4>
+                    {/* <button type="button" onClick={} */}
+                </div>
+                <div className="StockMarketVideo">
+                    <ResponsivePlayer give_url="https://www.youtube.com/watch?v=o4jfBC0AgIM"></ResponsivePlayer>
+                </div>
+
+                
+
+            </div>
+            </>
+
     );
 };
 
